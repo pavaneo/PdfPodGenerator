268d6d6fb2e70af0acd6567fd4c7872a437578c1
Pavan Jain <pavanpeo@gmail.com>
2021-06-23 14:24:38 +0530

    Inital Commit

---
+++ PdfPodGenerator/PdfPodGenerator/PdfPodGenerator.h
@@ -0,0 +1,18 @@
+//
+//  PdfPodGenerator.h
+//  PdfPodGenerator
+//
+//  Created by Pavan Jain on 23/06/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PdfPodGenerator.
+FOUNDATION_EXPORT double PdfPodGeneratorVersionNumber;
+
+//! Project version string for PdfPodGenerator.
+FOUNDATION_EXPORT const unsigned char PdfPodGeneratorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PdfPodGenerator/PublicHeader.h>
+
+
