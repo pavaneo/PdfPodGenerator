64b610f47555561e1b07feb1ce0c0ec8de84988a
Pavan Jain <pavanpeo@gmail.com>
2021-07-05 15:06:42 +0530

    Created new bundle class file

---
+++ PdfPodGenerator/bundle.swift
@@ -0,0 +1,15 @@
+//
+//  bundle.js
+//  PdfPodGenerator
+//
+//  Created by Pavan Jain on 05/07/21.
+//
+
+import Foundation
+
+class MainBundle {
+    
+    init() {
+        Print("Hello World")
+    }
+}
